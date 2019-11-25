b9e75e0330c3a3f4a24f43d5a71ac75ae38faf0b
demoyf <425746853@qq.com>
2019-11-25 14:56:47 +0800

    replace

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import FontLoader from 'three/src/loaders/FontLoader';
+import './src/opentype.src';
+import './src/worker.src';
 const THREE = {}
 THREE.FontLoader = FontLoader;
-import './src/opentype.src';
-
-import './src/worker.src';
